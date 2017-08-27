60a2e71721917eed81a79aeac060bb1773fedc1d
istewart <istewart3141@gmail.com>
2017-08-27 15:10:10 -0700

    Added a navbar.

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Container = styled.ul`
+  background-color: #333;
+  list-style-type: none;
+  position: fixed;
+  display: flex;
+  margin: 0;
+  padding: 0 10%;
+  width: 100%;
+`;
+
+const StyledMenuItem = styled.li`padding: .75em 1em;`;
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
+
+const Navbar = ({ articles }) =>
+  <Container>
+    <StyledMenuItem>
+      <StyledLink to="/">Home</StyledLink>
+    </StyledMenuItem>
+    <StyledMenuItem>
+      <StyledLink to="/about">About</StyledLink>
+    </StyledMenuItem>
+  </Container>;
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Navbar from './components/Navbar';
 import Temp from './components/Temp';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store/store';
@@ -12,6 +13,7 @@ render(
   <Provider store={store}>
     <Router>
       <div>
+        <Navbar />
         <Route path="/temp" component={Temp} />
         <Route path="/" component={App} />
       </div>
