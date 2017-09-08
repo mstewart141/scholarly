98b61b9cefabec1636ed7832a61a3fb41c74e53a
Michael (Stu) Stewart <mstewart141@gmail.com>
2017-09-08 09:26:30 -0700

    Add redux-logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunkMiddleware))
+  composeWithDevTools(applyMiddleware(thunkMiddleware, logger))
 );
 
 export default store;
